e63e6c7b48ac72e7de0c17ea741040a146398174
Petar Parov <petarparov@gmail.com>
2023-09-27 21:43:12 +0300

    added new solutions

---
+++ Projects/Fundamentals Lessons/src/ArraysLab_L03/EqualArrays_06.java
@@ -0,0 +1,12 @@
+package ArraysLab_L03;
+
+import java.util.Scanner;
+
+public class EqualArrays_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
---
+++ Projects/Fundamentals Lessons/src/ArraysLab_L03/SumEvenNumbers_03.java
@@ -0,0 +1,10 @@
+package ArraysLab_L03;
+
+import java.util.Scanner;
+
+public class SumEvenNumbers_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
