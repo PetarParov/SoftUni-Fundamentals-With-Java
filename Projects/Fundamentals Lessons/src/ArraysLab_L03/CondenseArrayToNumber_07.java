b0895b66b8d6f779c06e86f7201092e9bde24897
Petar Parov <petarparov@gmail.com>
2023-09-28 14:33:02 +0300

    modified some solutions

--- Projects/Fundamentals Lessons/src/ArraysLab_L03/CondenseArrayToNumber_07.java
+++ Projects/Fundamentals Lessons/src/ArraysLab_L03/CondenseArrayToNumber_07.java
@@ -6,5 +6,21 @@ public class CondenseArrayToNumber_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split(" ");
+        int[] array = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            array[i] = Integer.parseInt(input[i]);
+        }
+
+        while (array.length > 1) {
+            int[] condensed = new int[array.length - 1];
+            for (int i = 0; i < condensed.length; i++) {
+                condensed[i] = array[i] + array[i + 1];
+            }
+            array = condensed;
+        }
+
+        System.out.println(array[0]);
     }
 }
--- Projects/Fundamentals Lessons/src/ArraysLab_L03/SumEvenNumbers_03.java
+++ Projects/Fundamentals Lessons/src/ArraysLab_L03/SumEvenNumbers_03.java
@@ -1,10 +1,25 @@
 package ArraysLab_L03;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumEvenNumbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int [] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int sum = 0;
+
+        for (int number : numbers){
+            if (number % 2 == 0){
+                sum += number;
+            }
+        }
+        System.out.println(sum);
 
     }
 }
